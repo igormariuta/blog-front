381a526e75a9f5f8bd5d951440aa85469ebac647
Igor Mariua <igor.mariuta@gmail.com>
2022-03-10 23:50:22 +0200

    swr

--- src/pages/New/New.tsx
+++ src/pages/New/New.tsx
@@ -1,11 +1,19 @@
+import useSWR from "swr";
 import ArticleList from "../../components/ArticleList/ArticleList";
 
-function New() {
+const New = () => {
+  const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+  const { articles, error } = useSWR(
+    "http://localhost:1337/api/posts",
+    fetcher
+  );
+
   return (
     <div>
       <ArticleList />
     </div>
   );
-}
+};
 
 export default New;
